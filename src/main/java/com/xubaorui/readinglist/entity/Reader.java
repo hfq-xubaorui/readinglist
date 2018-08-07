2fca5d405141f11db991f8383d176bc0dbde017e
hfq-xubaorui <xubaorui@huizhaofang.com>
2018-08-07 08:43:30 +0800

    自定义配置（错误版本）

---
+++ src/main/java/com/xubaorui/readinglist/entity/Reader.java
@@ -0,0 +1,68 @@
+package com.xubaorui.readinglist.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public class Reader implements UserDetails {
+    private static final long serialVersionUID = 1L;
+    @Id
+    private String username;
+    private String fullname;
+    private String password;
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority("READER"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/xubaorui/readinglist/repository/ReaderRepository.java
@@ -0,0 +1,7 @@
+package com.xubaorui.readinglist.repository;
+
+import com.xubaorui.readinglist.entity.Reader;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReaderRepository extends JpaRepository<Reader,String> {
+}
